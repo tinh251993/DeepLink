9edd4a5d13f6a03c64a38a07c9eab8c390a2ae47
My Name <my-name@chromium.org>
2023-06-05 20:16:45 +0700

    add main

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+#ifdef __APPLE__
+#include <CoreFoundation/CoreFoundation.h>
+#endif
+
+int main(int argc, char** argv){
+    #ifdef _WIN32
+    
+}
