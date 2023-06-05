d45979b58777e2c6c2d99673b17ada33c8f4fc6a
My Name <my-name@chromium.org>
2023-06-05 20:37:24 +0700

    Add register to regedit

--- src/main.cpp
+++ src/main.cpp
@@ -12,5 +12,17 @@
 
 int main(int argc, char** argv){
     #ifdef _WIN32
+
+        const std::string customProtocol = "fruit"; //app name
+        const std::string appPath = "G:\fruit.exe"; //applink (later will take from registry)
+
+        std::string registryKey = "HKEY_CLASSES_ROOT\\" + customProtocol;  //registry custom link
+        RegSetKeyValueA(HKEY_CLASSES_ROOT, customProtocol.c_str(), nullptr, REG_SZ,
+         "URL:Custom Protocol", customProtocol.length());
+        RegSetKeyValueA(HKEY_CLASSES_ROOT, customProtocol.c_str(), "URL Protocol", REG_SZ, nullptr, 0);
+        RegSetKeyValueA(HKEY_CLASSES_ROOT, customProtocol.c_str(), "shell\\open\\command", REG_SZ,
+        (appPath + " \"%1\"").c_str(), (appPath + " \"%1\"").length());
+    #endif
     
+    return 0;
 }
